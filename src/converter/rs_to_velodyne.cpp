3ee14998d183e54daef2e3b2c6ff09b4b04ffa48
Zihan Lin <linzihan02@megvii.com>
2023-08-25 14:19:34 +0800

    [fix]: replace pcl_isnan with std::isnan
     according to:
    * **[modernization]** Deprecate `pcl_isnan`, `pcl_isfinite`, and `pcl_isinf` in favor of `std` methods [[#2798](https://github.com/PointCloudLibrary/pcl/pull/2798), [#3457](https://github.com/PointCloudLibrary/pcl/pull/3457)]

--- src/converter/rs_to_velodyne.cpp
+++ src/converter/rs_to_velodyne.cpp
@@ -79,10 +79,10 @@ bool has_nan(T point) {
     // remove nan point, or the feature assocaion will crash, the surf point will containing nan points
     // pcl remove nan not work normally
     // ROS_ERROR("Containing nan point!");
-    if (pcl_isnan(point.x) || pcl_isnan(point.y) || pcl_isnan(point.z)) {
-        return true;
+    if (std::isnan(point.x) || std::isnan(point.y) || std::isnan(point.z)) {
+      return true;
     } else {
-        return false;
+      return false;
     }
 }
 
